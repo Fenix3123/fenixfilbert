d8e93c7a66f6d24aa02109e4f9c11b0e39b35025
Fenix3123 <63925178+Fenix3123@users.noreply.github.com>
2021-06-02 11:17:13 -0500

    commit
    commit

---
+++ Javascript/Mixed_messages.js
@@ -0,0 +1,12 @@
+let word1 = ["Hi", "Hello"];
+let word2 = ["me", "My"];
+let word3 = ["calling", "name"];
+let word4 = ["is", "am"];
+let word5 = ["Fenix", "John"];
+
+var message = prompt("What sort of message do you want?");
+
+console.log(`${word1[Math.floor(Math.random() * word1.length)]} ${word2[Math.floor(Math.random() * word2.length)]} ${word3[Math.floor(Math.random() * word3.length)]} ${word4[Math.floor(Math.random() * word4.length)]} ${word5[Math.floor(Math.random() * word5.length)]}`);
+
+console.log
+
